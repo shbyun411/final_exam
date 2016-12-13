24c9b2628e9aae0912349e921f419426f58c8b9c
Ren-bravery <renyuuki64@gmail.com>
2016-12-13 21:03:09 +0900

    20141326

--- Problem1/src/main/java/com/example/igx/problem1/MainActivity.java
+++ Problem1/src/main/java/com/example/igx/problem1/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.igx.problem1;
 
+import android.hardware.Sensor;
+import android.location.LocationListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.location.Location;
+
+import static android.content.Intent.ACTION_SENDTO;
+import static android.hardware.Sensor.STRING_TYPE_ACCELEROMETER;
+import static android.provider.ContactsContract.Intents.Insert.ACTION;
 
 public class MainActivity extends AppCompatActivity /* implements Something1, Something2 */ {
 
@@ -22,9 +29,17 @@ public class MainActivity extends AppCompatActivity /* implements Something1, So
         final TextView text_selectedType = (TextView) findViewById(R.id.text_selectedType);
         final EditText edit_phoneNumber = (EditText) findViewById(R.id.edit_phoneNumber);
 
+
         btn_getLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                text_selectedType.setText("Location");
+
+
+                double a = location.getLongitude();
+                double b = location.getAltitude();
+                double c = location.getLatitude();
+                text_selectedData.setText("Longitude = "+Double.toString(a) + "Altitude = "+Double.toString(b) + "Latitude = "+Double.toString(c));
 
             }
         });
@@ -32,6 +47,13 @@ public class MainActivity extends AppCompatActivity /* implements Something1, So
         btn_getSensors.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                text_selectedType.setText("Sensors");
+
+                String x = STRING_TYPE_ACCELEROMETER;
+
+                text_selectedData.setText("STRING_TYPE_ACCELEROMETER = " + x);
+
+
 
             }
         });
@@ -40,7 +62,15 @@ public class MainActivity extends AppCompatActivity /* implements Something1, So
             @Override
             public void onClick(View v) {
 
+                ACTION_SENDTO
+
             }
         });
     }
+
+
+
+    Location location;
+    LocationListener locationListener;
+    Sensor sensor;
 }
